bb2b2ae33776f7559489d1565e83726ace3280d5
ram-bob <rbmandadapu@gmail.com>
2017-02-21 17:59:18 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask import json
+from flask import jsonify
+import sqlite3
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+@app.route("/posts", methods=['GET'])
+def post():
+    conn = sqlite3.connect('blog.db')
+    cursor = conn.execute("SELECT post_id, title, body from posts")
+    list=[]
+    for row in cursor:
+      list.append({'post_id': row[0] , 'title': row[1], 'body': row[2]})
+    conn.close()
+    return jsonify(results=list)
+
+if __name__ == "__main__":
+    app.run()
